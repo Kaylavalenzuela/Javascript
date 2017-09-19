a840c370cbc22d56d3f602e8650f066d30d109ec
Kaylavalenzuela <kaylalaneevalenzuela@gmail.com>
2017-09-19 14:17:23 -0700

    assignment 1

---
+++ js/script.js
@@ -0,0 +1,17 @@
+//
+// var.why = "I wanted to take this class to learn more about different languages and really gain experience to eventually join cohort and become a part of the Bitwise team. I also want to learn hor to create an app and work on my design skills");
+// console.log ("why");
+
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<script>
+var why = ("why");
+console.log ("I want to take this class to learn another language and improve my coding to eventually join the cohort")
+</script>
+
+<script src="javascript/js/script.js"></script>
+</body>
+</html>
